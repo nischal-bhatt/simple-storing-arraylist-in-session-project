10eeb7d30bb3d2ee1505c9afce0d4e937844d5dd
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-01-23 18:45:38 +0800

    asd

--- demo-porject-13/src/main/java/com/example/demoporject/SessionController.java
+++ demo-porject-13/src/main/java/com/example/demoporject/SessionController.java
@@ -3,6 +3,7 @@ package com.example.demoporject;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,11 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 public class SessionController {
 
+	@Autowired
+	HttpSession s;
 	
 	@GetMapping("/hey")
-	public String hi (HttpSession s)
+	public String hi ()
 	{
 		s.setAttribute("nbi", 29);
 		List<String> strings = new ArrayList<>();
